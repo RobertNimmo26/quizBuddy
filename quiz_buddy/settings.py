b1ade34d08ebb644db98e832e4db60a8cf4657e9
Archie Greogry <2399335g@student.gla.ac.uk>
2020-03-06 15:51:34 +0000

    Added local bootstrap file, navBar button block ({% block header_buttons %}

--- quiz_buddy/settings.py
+++ quiz_buddy/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'twitter_bootstrap',
+    'twitter_bootstrap'
 ]
 
 MIDDLEWARE = [
