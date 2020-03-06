ff25c0c2608ebfb6303d34f9a260bcbc967d11b2
Robert Nimmo <2379875n@student.gla.ac.uk>
2020-03-06 13:26:29 +0000

    creating models

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from quiz.models import User,QuizTaker,Character
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email','username','name','is_teacher','is_student')
+
+class QuizTakerAdmin(admin.ModelAdmin):
+    list_display = ('correctAnswers','is_completed')
+
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ('characterType','evolutionStage','evolveScore')
+
+
 
 # Register your models here.
+
+admin.site.register(User, UserAdmin)
+admin.site.register(QuizTaker, QuizTakerAdmin)
+admin.site.register(Character, CharacterAdmin)
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
-# Create your models here.
+from .managers import CustomUserManager
+
+class Character(models.Model):
+    characterType = models.IntegerField(_("type"))
+    evolutionStage = models.IntegerField(_("stage"),default=1)
+    evolveScore = models.IntegerField(_("score"),default=0)
+
+class QuizTaker(models.Model):
+    #user = models.ForeignKey("User", on_delete=models.CASCADE) #---Need to connect
+    #quiz = models.ForeignKey("Quiz", on_delete=models.CASCADE) #---Need to connect
+    correctAnswers = models.IntegerField(_("correct answers"))
+    is_completed = models.BooleanField(_("completed"))
+    #timestamp = models.models.DateTimeField(auto_now_add=True, blank=True)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(_("user name"), max_length=30)
+    email = models.EmailField(_('email address'), unique=True)
+    name = models.CharField(_("name"), max_length=40)
+    is_admin = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+    character = models.ForeignKey("Character", blank=True, null=True, on_delete=models.SET_NULL) #---Need to connect
+
+    #Required django User fields
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    #####
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
