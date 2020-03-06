889708321de2b9749e0c98b199ec6523bf6ba046
Robert Nimmo <2379875n@student.gla.ac.uk>
2020-03-06 14:39:58 +0000

    merged models

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from quiz.models import User,QuizTaker,Character, Quiz, Question
+from quiz.models import User,QuizTaker,Character, Quiz, Question, Class, Option
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('email','username','name','is_teacher','is_student')
+
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ('classId','name','get_teachers','get_students')
 
 class QuizTakerAdmin(admin.ModelAdmin):
     list_display = ('correctAnswers','is_completed')
@@ -16,6 +19,9 @@ class QuizAdmin(admin.ModelAdmin):
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('text', 'quiz')
 
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'question', 'is_correct')
+
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
@@ -23,3 +29,5 @@ admin.site.register(QuizTaker, QuizTakerAdmin)
 admin.site.register(Character, CharacterAdmin)
 admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Class, ClassAdmin)
+admin.site.register(Option, OptionAdmin)
--- quiz/models.py
+++ quiz/models.py
@@ -7,19 +7,24 @@ from django.utils import timezone
 
 from .managers import CustomUserManager
 
+# Character model
 class Character(models.Model):
-    characterType = models.IntegerField(_("type"))
+    characterType = models.IntegerField(_("type"),default=1)
     evolutionStage = models.IntegerField(_("stage"),default=1)
     evolveScore = models.IntegerField(_("score"),default=0)
 
+    def __str__(self):
+        return str(self.characterType)
+
+# User model
 class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(_("user name"), max_length=30)
+    username = models.CharField(_("user name"), max_length=50, unique=True)
     email = models.EmailField(_('email address'), unique=True)
-    name = models.CharField(_("name"), max_length=40)
+    name = models.CharField(_("name"), max_length=50)
     is_admin = models.BooleanField(default=False)
     is_teacher = models.BooleanField(default=False)
     is_student = models.BooleanField(default=False)
-    character = models.ForeignKey("Character", blank=True, null=True, on_delete=models.SET_NULL) #---Need to connect
+    character = models.ForeignKey("Character", blank=True, null=True, on_delete=models.SET_NULL)
 
     #Required django User fields
     is_staff = models.BooleanField(default=False)
@@ -34,6 +39,25 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
+class Class(models.Model):
+    classId = models.AutoField(primary_key=True,verbose_name=_("id"))
+    name = models.CharField(_("name"), max_length=50)
+    teacher = models.ManyToManyField("User", verbose_name=_("teacher"),related_name="teachers")
+    student = models.ManyToManyField("User", verbose_name=_("student"),related_name="students")
+
+    def get_teachers(self):
+        return ",".join([str(t) for t in self.teacher.all()])
+
+    def get_students(self):
+        return ",".join([str(s) for s in self.student.all()])
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        # Fix pluralization of model name
+        verbose_name_plural = 'Classes'
+
 # Quiz Model
 class Quiz(models.Model):
     name = models.CharField(unique=True, max_length=50)
@@ -42,22 +66,22 @@ class Quiz(models.Model):
     description = models.CharField(max_length=255)
     due_date = models.DateTimeField(auto_now_add=False)
     question_count = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
         # Fix pluralization of model name
         verbose_name_plural = 'Quizzes'
-    
+
 # Question model
 class Question(models.Model):
     text = models.CharField(unique=True, max_length=50)
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-     
+
     def __str__(self):
         return self.text
-    
+
 # Option Model
 class Option(models.Model):
     text = models.CharField(unique=True, max_length=50)
@@ -74,6 +98,6 @@ class QuizTaker(models.Model):
     correctAnswers = models.IntegerField(default=0)
     is_completed = models.BooleanField(default=False)
     timestamp = models.DateTimeField()
-    
+
     def __str__(self):
         return self.user
