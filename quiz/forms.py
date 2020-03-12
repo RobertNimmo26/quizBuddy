0d4563aca20778e496b5f51a8f507df2315c4877
Craig Murray <craig.murray095@gmail.com>
2020-03-12 15:52:56 +0000

    Started work on quizForm and create-quiz template

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,11 +1,21 @@
 from django import forms
+from django.forms import formset_factory
 from quiz.models import Question, Quiz, Option
 
+class questionForm(forms.ModelForm):
+    questionName = forms.CharField(max_length=50, required=True)
 
+class quizForm(forms.ModelForm):
+    name = forms.CharField(max_length=50, required=True)
+    description = forms.CharField(max_length=255, required=False)
+    due_date = forms.DateTimeField(required=True)
+    question_count = forms.IntegerField(default=0)
+   
+    class Meta:
+       model = Quiz
+       fields = ('name', 'description', 'due_date')
+       
 class quizCreationForm(forms.ModelForm):
-    
-
-    # Form info
     class Meta:
         model = Quiz
         fields = ()
