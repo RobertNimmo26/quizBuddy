b7bc39e35b1b31660171ef083f368a15cc9faf7f
ivonnmolnar <2376241m@student.gla.ac.uk>
2020-03-13 15:55:56 +0000

    reg/login works, character choosing is currently excluded from registration, about page somewhat ready

--- quiz/views.py
+++ quiz/views.py
@@ -49,97 +49,61 @@ def preferences(request):
 
 
 def registerStudent(request):
-    '''context_dict = {}
-    # If the request is a HTTP POST, try to pull out the relevant information.
-    if request.method == 'POST':
-        # get email and password from form
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        character = request.POST.get('radio')
-        create_user(email, password, username, is_student=True,  character=character)
-
-    else:
-        return render(request, 'register-student.html')'''
-
     registered = False
 
-    # If it's a HTTP POST, we're interested in processing form data.
+    # means they are trying to register so we have to save the data
     if request.method == 'POST':
 
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
+        # grabs data from form
         user_form = UserFormStudent(request.POST)
 
-        # If the two forms are valid...
         if user_form.is_valid():
-        # Save the user's form data to the database.
+            # save the user's form data to the database
             user = user_form.save()
 
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
+            # hash the password with the set_password method and update the user object
             user.set_password(user.password)
             user.save()
 
-            # Update our variable to indicate that the template
-            # registration was successful.
             registered = True
         else:
-            # Invalid form or forms - mistakes or something else?
-            # Print problems to the terminal.
+            # invalid form, print error
             print(user_form.errors)
     else:
-        # Not a HTTP POST, so we render our form using two ModelForm instances.
-        # These forms will be blank, ready for user input.
         user_form = UserFormStudent()
 
-    # Render the template depending on the context.
     return render(request, 'register-student.html', context = {'user_form': user_form, 'registered': registered})
 
 
 def registerTeacher(request):
     registered = False
 
-    # If it's a HTTP POST, we're interested in processing form data.
     if request.method == 'POST':
 
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
         user_form = UserFormTeacher(request.POST)
 
-        # If the two forms are valid...
         if user_form.is_valid():
-        # Save the user's form data to the database.
+
             user = user_form.save()
 
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
             user.set_password(user.password)
             user.save()
 
-            # Update our variable to indicate that the template
-            # registration was successful.
             registered = True
         else:
-            # Invalid form or forms - mistakes or something else?
-            # Print problems to the terminal.
             print(user_form.errors)
     else:
-        # Not a HTTP POST, so we render our form using two ModelForm instances.
-        # These forms will be blank, ready for user input.
         user_form = UserFormTeacher()
 
-    # Render the template depending on the context.
     return render(request, 'register-teacher.html', context = {'user_form': user_form, 'registered': registered})
 
 def user_login(request):
     context_dict = {}
-    # If the request is a HTTP POST, try to pull out the relevant information.
+    # if post, means they are logging in
     if request.method == 'POST':
-        # get email and password from form
-        print(request.body)
         email = request.POST.get('email')
         password = request.POST.get('password')
+
         # Django's auth
         user = authenticate(email=email, password=password)
         # If there's a match
@@ -155,7 +119,6 @@ def user_login(request):
             print(f"Invalid login details: {email}, {password}")
             context_dict['error'] = "Invalid login details supplied."
             return render(request, 'index.html', context=context_dict)
-    # The request is not a HTTP POST, so display the login form.
-    # This scenario would most likely be a HTTP GET.
+
     else:
         return render(request, 'index.html', context=context_dict)
