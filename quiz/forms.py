2ce75f526cc3f8d065dffbd113b204fe5f930011
ivonnmolnar <2376241m@student.gla.ac.uk>
2020-03-16 15:34:04 +0000

    hopefully this works

--- quiz/forms.py
+++ quiz/forms.py
@@ -6,14 +6,14 @@ class UserFormStudent(forms.ModelForm):
     #CHAR_CHOICES =(("1", "Char1"), ("2", "Char2"), ("3", "Char3"))
     password = forms.CharField(widget=forms.PasswordInput())
     #character = forms.ChoiceField(widget=forms.RadioSelect(), choices=CHAR_CHOICES)
-    is_student = forms.CharField(widget=forms.HiddenInput(), initial=True, required=False)
+    is_student = forms.CharField(widget=forms.HiddenInput(), initial=True)
     class Meta:
         model = User
         fields = ('username', 'name', 'email', 'password', 'is_student')
 
 class UserFormTeacher(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(), required=False, initial=True)
-    is_teacher = forms.BooleanField(widget=forms.HiddenInput(), initial=True, required=False)
+    is_teacher = forms.CharField(widget=forms.HiddenInput(), initial=True)
     class Meta:
         model = User
         fields = ('username', 'name', 'email', 'password', 'is_teacher')
