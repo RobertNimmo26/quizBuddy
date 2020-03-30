f76fdacc5072e106bba9ae01f10dacb90956d8f9
Wafa Khan Tareen <2366702t@student.gla.ac.uk>
2020-03-30 22:16:14 +0100

    Update settings.py

--- quiz_buddy/settings.py
+++ quiz_buddy/settings.py
@@ -126,6 +126,7 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 
 AUTH_USER_MODEL = 'quiz.User'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT= 587
 EMAIL_USE_TLS = True
 
