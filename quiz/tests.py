0b491921a496c4de2daddef4808e0f47bf1bab4c
ivonnmolnar <2376241m@student.gla.ac.uk>
2020-04-01 16:27:41 +0100

    Form test cases done
    - Mainly checks for labels being correct
    - And checks if querysets have the correct data
    - Doesn't check for validation as the documentation said you shouldn't need to do so so uh yeah

--- quiz/tests.py
+++ quiz/tests.py
@@ -1,3 +1,98 @@
 from django.test import TestCase
+from django.utils import timezone
+from quiz.forms import UserFormStudent, UserFormTeacher, quizCreationForm, questionFormset, QuizLibrary, classCreationForm
+from quiz.models import Character,User,Class, Quiz, Question, Option, QuizTaker
+from quiz.managers import CustomUserManager
+import random
 
-# Create your tests here.
+#FORM TESTING - based on https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing
+class UserFormStudentTest(TestCase):
+    def test_password_label(self):
+        form = UserFormStudent()
+        self.assertTrue(form.fields['password'].label == None or form.fields['password'].label == 'Password')
+
+    def test_username_label(self):
+        form = UserFormStudent()
+        self.assertTrue(form.fields['username'].label == None or form.fields['username'].label == 'User name')
+
+    def test_name_label(self):
+        form = UserFormStudent()
+        self.assertTrue(form.fields['name'].label == None or form.fields['name'].label == 'Name')
+
+    def test_email_label(self):
+        form = UserFormStudent()
+        self.assertTrue(form.fields['email'].label == None or form.fields['email'].label == 'Email address')
+
+class UserFormTeacherTest(TestCase):
+    def test_password_label(self):
+        form = UserFormTeacher()
+        self.assertTrue(form.fields['password'].label == None or form.fields['password'].label == 'Password')
+
+    def test_username_label(self):
+        form = UserFormTeacher()
+        self.assertTrue(form.fields['username'].label == None or form.fields['username'].label == 'User name')
+
+    def test_name_label(self):
+        form = UserFormTeacher()
+        self.assertTrue(form.fields['name'].label == None or form.fields['name'].label == 'Name')
+
+    def test_email_label(self):
+        form = UserFormTeacher()
+        self.assertTrue(form.fields['email'].label == None or form.fields['email'].label == 'Email address')
+
+class classCreationFormTest(TestCase):
+    def test_name_label(self):
+        form = classCreationForm()
+        self.assertTrue(form.fields['name'].label == None or form.fields['name'].label == 'Name')
+
+class quizCreationFormTest(TestCase):
+    def test_quiz_title_label(self):
+        form = quizCreationForm()
+        self.assertTrue(form.fields['quiz_title'].label == None or form.fields['quiz_title'].label == 'Quiz Title')
+
+    def test_quiz_description_label(self):
+        form = quizCreationForm()
+        self.assertTrue(form.fields['quiz_description'].label == None or form.fields['quiz_description'].label == 'Quiz Description')
+
+    def test_course_label(self):
+        form = quizCreationForm()
+        self.assertTrue(form.fields['course'].label == None or form.fields['course'].label == 'Class')
+
+    def test_course_queryset(self):
+        classVar = Class.objects.get_or_create(name="C")[0]
+        classVar.save()
+        form = quizCreationForm()
+        self.assertTrue(form.fields['course'].queryset.get() == classVar)
+
+    def test_due_date_label(self):
+        form = quizCreationForm()
+        self.assertTrue(form.fields['due_date'].label == None or form.fields['due_date'].label == 'Quiz Description')
+
+class QuizLibraryTest(TestCase):
+    def test_quiz_label(self):
+        form = QuizLibrary()
+        self.assertTrue(form.fields['quiz'].label == None or form.fields['quiz'].label == 'Quiz')
+
+    def test_quiz_queryset(self):
+        randomDay=random.randint(-5,20)
+        due_date = timezone.now() + timezone.timedelta(days=randomDay)
+        teacher = User.objects.create_user(email = "a@a.com", password = "aaaaaaaa", name = "a", username = "a", is_teacher = True, is_staff = True )
+        teacher.save()
+        quiz = Quiz(name = "A",description="A",due_date=due_date,question_count=2,teacher=teacher)
+        quiz.save()
+        form = QuizLibrary()
+        self.assertTrue(form.fields['quiz'].queryset.get() == quiz)
+
+    def test_course_label(self):
+        form = QuizLibrary()
+        self.assertTrue(form.fields['course'].label == None or form.fields['course'].label == 'Class')
+
+    def test_course_queryset(self):
+        classVar = Class.objects.get_or_create(name="C")[0]
+        classVar.save()
+        form = QuizLibrary()
+        self.assertTrue(form.fields['course'].queryset.get() == classVar)
+
+    def test_due_date_label(self):
+        form = QuizLibrary()
+        self.assertTrue(form.fields['due_date'].label == None or form.fields['due_date'].label == 'Due date')
